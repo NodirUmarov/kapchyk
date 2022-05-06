84e00754638314e947d05d12e6b51ae8d7b08e19
mruma <mr.umarov.1995@gmail.com>
2022-05-06 11:02:27 +0600

    .

--- src/main/java/kg/itschool/kapchyk/controller/WalletController.java
+++ src/main/java/kg/itschool/kapchyk/controller/WalletController.java
@@ -2,8 +2,10 @@ package kg.itschool.kapchyk.controller;
 
 import kg.itschool.kapchyk.model.request.CreateWalletRequest;
 import kg.itschool.kapchyk.service.WalletService;
+import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,10 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/wallet")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class WalletController {
 
-    @NonNull private WalletService walletService;
+    @NonNull WalletService walletService;
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody @Valid CreateWalletRequest request) {
